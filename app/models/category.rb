2ef62012a3b4118e3d67b809b6e46e8f9c387100
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-04-11 10:05:52 -0600

    added live stores and products to seed db

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name, :category_ids
+  attr_accessible :name, :category_ids, :store_id
   has_many :product_categories
   has_many :products, :through => :product_categories
   scope :by_name, order("name asc")
