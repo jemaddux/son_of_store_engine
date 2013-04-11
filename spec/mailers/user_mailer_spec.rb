1c51109d4b144ac0a737c1ee187feb8bf4628a58
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-04-11 09:21:47 -0600

    user confirmation url updated in confirmation email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,7 +4,7 @@ describe UserMailer do
   describe "order_confirmation" do
 
     let(:user_email) { "user@oregonsale.com" }
-    let(:order) { Order.create(confirmation: "AAAAAA") }
+    let(:order) { Order.create(confirmation: "AAAAAA", confirmation_hash: UUID.new.generate, card_number: '4242424242424242') }
     let(:mail) { UserMailer.order_confirmation(user_email, order) }
 
     it "renders the headers" do
@@ -17,8 +17,13 @@ describe UserMailer do
       mail.body.encoded.should match("AAAAAA")
     end
 
-    it "has a unique confirmation url" do 
-      expect(mail.body).to include(order.confirmation_url)
+    it "has that order's unique confirmation code" do 
+      expect(mail.body).to include(order.confirmation_hash)
+    end
+
+    it "has that order's unique confirmation url" do 
+      puts order.inspect
+      expect(mail.body.encoded).to match("mrblonde.herokuapp.com/orders/review/#{order.confirmation_hash}")
     end
   end
 end
