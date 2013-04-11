dd0c8ab22d14c1119ea1e337fef0128774000bbb
Aimee Maher <aimee@geekycheeks.com>
2013-04-10 21:57:23 -0600

    homepage fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def show
-    @categories = Category.all.sort
-    @products = Category.find_by_name("Essentials").products.shuffle[0..2]
     render layout: "home"
   end
 
