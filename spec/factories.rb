87fcc66be6ed830c75d25631f287a3eebe41f607
John Maddux <jemaddux@gmail.com>
2013-04-13 16:39:04 -0600

    fixing tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -39,7 +39,7 @@ FactoryGirl.define do
   factory :super_admin, class: User do
     full_name "user"
     email "test@test.com"
-    role "superuser"
+    role "platform_admin"
     password "password"
   end
 
