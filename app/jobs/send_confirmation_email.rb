17cc42fb7603df7dd2004aa10d762ddc341b5dae
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-04-14 17:44:08 -0600

    testing

--- app/jobs/send_confirmation_email.rb
+++ app/jobs/send_confirmation_email.rb
@@ -1,7 +1,7 @@
 class SendConfirmationEmail
   @queue = :confirmation_email
 
-  def self.perform 
-    UserMailer.order_confirmation(order_user.email, confirmation_code, confirmation_hash)
-  end
+  # def self.perform 
+  #   UserMailer.order_confirmation(order_user.email, confirmation_code, confirmation_hash)
+  # end
 end
