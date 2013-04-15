6bcd06ecd6a66314c7396599d3b0873399c990d5
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-04-15 14:23:27 -0600

    resqueue tested, working

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -71,6 +71,7 @@ describe OrdersController do
         end 
 
         it "allows that user to check out" do 
+          (Resque).should_receive(:enqueue)
           post :create,  card_number: '4242424242424242', user_email: "email@email.test"
           expect(Order.count).to eq 1
         end 
