df9b3fa426887a2f5d4d8b57f12a35bce053e016
John Maddux <jemaddux@gmail.com>
2013-04-17 09:48:06 -0600

    cleaning up

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,3 +6,4 @@ StoreEngine::Application.config.session_store :cookie_store, key: '_store_engine
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # StoreEngine::Application.config.session_store :active_record_store
+
